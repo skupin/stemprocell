a9d30b1c913e58566a4335cd5f830e963efbbce8
maxskupin@gmail.com <Kadmos.447713001>
2018-10-11 12:08:59 -0400

    testing acordion icon v2

--- tratamientos_clinicos.php
+++ tratamientos_clinicos.php
@@ -15,7 +15,7 @@
     <div id="accordion">
         <div class="card">
             <div class="card-header" id="clinicosOne">
-                <i class="fas fa-angle-right"> </i>
+                <span class="fas fa-angle-right"></span>
                 <h5 class="mb-0">
                     <button class="btn btn-link" data-toggle="collapse" data-target="#clinicosCollapseOne"
                             aria-expanded="false" aria-controls="clinicosCollapseOne">
